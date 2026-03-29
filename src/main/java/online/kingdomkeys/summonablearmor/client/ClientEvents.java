92488f258214558616d58c665573bac53511110a
Abel <abel54225@gmail.com>
2026-03-29 11:00:44 +0200

    Added debug to summoned armor

--- src/main/java/online/kingdomkeys/summonablearmor/client/ClientEvents.java
+++ src/main/java/online/kingdomkeys/summonablearmor/client/ClientEvents.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.ArmorItem;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 import online.kingdomkeys.summonablearmor.ModComponents;
+import online.kingdomkeys.summonablearmor.Utils;
 
 public class ClientEvents {
     @SubscribeEvent
@@ -13,6 +14,10 @@ public class ClientEvents {
         if(event.getItemStack().has(ModComponents.ARMOR_ID)){
             if(event.getItemStack().getItem() instanceof ArmorItem) {
                 event.getToolTip().add(1, Component.translatable("summonablearmor.summoned").withStyle(ChatFormatting.GOLD));
+                if(event.getFlags().isAdvanced()){
+                    event.getToolTip().add(2, Component.literal("DEBUG:").withStyle(ChatFormatting.RED));
+                    event.getToolTip().add(3, Component.literal(Utils.getArmorID(event.getItemStack())+""));
+                }
             }
         }
     }
