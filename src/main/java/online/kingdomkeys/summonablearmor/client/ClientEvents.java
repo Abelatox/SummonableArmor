25ff9a06914fa43b73d5b6a8cabe5a6f6da5d7ca
Abel <abel54225@gmail.com>
2026-03-29 01:18:22 +0100

    Only add tooltip to ArmorItem

--- src/main/java/online/kingdomkeys/summonablearmor/client/ClientEvents.java
+++ src/main/java/online/kingdomkeys/summonablearmor/client/ClientEvents.java
@@ -2,6 +2,7 @@ package online.kingdomkeys.summonablearmor.client;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ArmorItem;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 import online.kingdomkeys.summonablearmor.ModComponents;
@@ -9,8 +10,10 @@ import online.kingdomkeys.summonablearmor.ModComponents;
 public class ClientEvents {
     @SubscribeEvent
     public void clientTickPre(ItemTooltipEvent event) {
-        if(event.getItemStack().has(ModComponents.ARMOR_ID)){
-            event.getToolTip().add(1,Component.literal(ChatFormatting.GOLD+"SUMMONED"));
+        if(event.getItemStack() != null && event.getItemStack().has(ModComponents.ARMOR_ID)){
+            if(event.getItemStack().getItem() instanceof ArmorItem) {
+                event.getToolTip().add(1, Component.literal(ChatFormatting.GOLD + "SUMMONED"));
+            }
         }
     }
 
